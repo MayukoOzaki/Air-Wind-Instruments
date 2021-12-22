5ab69ec6e65a6912ba827c51732ae9a2b8c9654e
IDEHARA Norimichi <idehara@tama.ac.jp>
2021-12-22 18:31:37 +0900

    New prefab based on the Saito-Model

--- dandelion/application-video/Assets/DandelionModels/Scripts/DandelionWalker.cs
+++ dandelion/application-video/Assets/DandelionModels/Scripts/DandelionWalker.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 
 public class DandelionWalker : MonoBehaviour
 {
-    public GameObject camera;
+    GameObject camera;
     bool isBlown;
     // Start is called before the first frame update
     void Start()
